ed8df0edc99ca98164a25e286b149df55d42525f
SimoneVilardo <simovila95@gmail.com>
2023-06-29 11:36:34 +0200

    milestone 2 completata

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -15,9 +15,10 @@ export default {
 </script>
 <template>
     <div>
-        <h1>{{ myFilm.title }}</h1>
-        <h2>{{ myFilm.original_title }}</h2>
-        <h3>{{ myFilm.original_lenguage }}</h3>
+        <h1>{{ myFilm.title || myFilm.name }}</h1>
+        <h2>{{ myFilm.original_title || myFilm.original_name }}</h2>
+        <h3> <img :src="`../../node_modules/flagpack/flags/1x1/${myFilm.original_language}.svg`" alt=""></h3>
+        <h3>{{ myFilm.original_language }}</h3>
         <h4>{{ myFilm.vote_average }}</h4>
     </div>
 </template>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from 'vue';
 
 export const store = reactive({
-    apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=c18732d723a3337a0306011f05d8a7b5',
+    apiUrl: 'https://api.themoviedb.org/3/search/multi?api_key=c18732d723a3337a0306011f05d8a7b5',
     myFilms: [],
     filmsValue: ''
 })
